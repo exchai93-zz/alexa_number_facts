fb87993dc6e0b64932020b9ae7b448d6ec466b1c
Emily Chai <echai93@gmail.com>
2017-05-27 15:01:12 +0100

    Implement customised slot value fact_type

--- alexa-number-facts/server.rb
+++ alexa-number-facts/server.rb
@@ -4,11 +4,13 @@ require 'net/http'
 
 post '/' do
 
+  parsed_request = JSON.parse(request.body.read)
   # grab the slot value from the incoming request
-  number = JSON.parse(request.body.read)["request"]["intent"]["slots"]["Number"]["value"]
+  number = parsed_request["request"]["intent"]["slots"]["Number"]["value"]
+  fact_type = parsed_request["request"]["intent"]["slots"]["FactType"]["value"]
 
   # pass that number to the numbers api
-  number_facts_uri = URI("http://numbersapi.com/#{ number }")
+  number_facts_uri = URI("http://numbersapi.com/#{ number }/#{  fact_type }")
   number_fact = Net::HTTP.get(number_facts_uri)
 
   {
